3618afd185e7919b58ee19a7a600d23362db5259
Aman Desai <amandesai291@gmail.com>
2026-08-19 09:54:51 +0530

    Time: 5 ms (42.47%), Space: 94.4 MB (67.75%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,6 +9,53 @@
  * }
  */
 class Solution {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode fwd = null;
+
+        while(curr != null)
+        {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+
+        return prev;
+
+    }
+    public boolean isPalindrome(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        head2 = reverseList(head2);
+
+        ListNode i = head;
+        ListNode j = head2;
+        while(j!=null)
+        {
+            if(i.val != j.val) return false;
+            i = i.next;
+            j = j.next;
+          
+        }
+        return true;
+    }
+
+    /*
     public boolean isPalindrome(ListNode head) {
         ListNode temp = head;
 
@@ -33,4 +80,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
